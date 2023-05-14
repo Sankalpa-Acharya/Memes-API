ddd07273cf8538bc6e6b4515082edf0c6446a816
sankalpa-acharya <sankalpaacharya6609@gmail.com>
2023-05-14 13:27:23 +0530

    edit:port change

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ import express from 'express'
 import homeRouter from './routes/memesRouter.js'
 
 const app = express()
-
+const port = process.env.PORT || 3000
 app.set('view engine','ejs')
 app.use('/',homeRouter)
 app.use(express.static('public'));
 
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("listening at port 3000 \nhttp://localhost:3000")
 })
