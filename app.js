94ec9c8214770110077969dfb08bf036472a4c23
sankalpa-acharya <sankalpaacharya6609@gmail.com>
2023-10-26 13:23:12 +0530

    fix: imported CORS which was not imported

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-import express from 'express'
-import homeRouter from './routes/memesRouter.js'
+import express from 'express';
+import cors from 'cors'; 
+import homeRouter from './routes/memesRouter.js';
 
 const app = express()
 const port = process.env.PORT || 3000
